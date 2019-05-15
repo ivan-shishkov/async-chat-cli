73344f51f47b2512b4b7117821cc23046187bc0b
Ivan Shishkov <mail-shishkov@yandex.ru>
2019-05-15 22:36:57 +0300

    Added module with common functions

---
+++ utils.py
@@ -0,0 +1,11 @@
+import asyncio
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def open_connection(host, port):
+    reader, writer = await asyncio.open_connection(host=host, port=port)
+    try:
+        yield reader, writer
+    finally:
+        writer.close()
